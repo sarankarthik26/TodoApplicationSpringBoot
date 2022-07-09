f3cb910fa069fc286b23a13489ea3920e5b4e2b6
Saran Karthik E <sarankarthik@Sarans-MacBook-Pro.local>
2022-07-09 17:04:41 +0530

    Added login and logout for the application backend

--- src/main/java/com/example/todo/config/SecurityConfig.java
+++ src/main/java/com/example/todo/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.example.todo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -11,9 +15,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/h2-console/**").permitAll();
-
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated();
         http.csrf().ignoringAntMatchers("/h2-console/**");
         http.headers().frameOptions().sameOrigin();
+
+        http.formLogin()
+                .and().logout().logoutSuccessUrl("/login");
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("saran").password("saran").roles("ADMIN");
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
